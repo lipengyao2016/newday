c0b424cb8f8af456b43c940fd0d17483f6f158ad
lipengyao2019 <yao50cn@163.com>
2020-10-09 18:59:57 +0800

    1.添加日志插件。

--- src/main/java/com/tpw/newday/controller/UserMingXiController.java
+++ src/main/java/com/tpw/newday/controller/UserMingXiController.java
@@ -3,6 +3,8 @@ package com.tpw.newday.controller;
 import com.tpw.newday.bean.User;
 import com.tpw.newday.bean.UserMingxi;
 import com.tpw.newday.service.IUserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
+
 @RestController
 public class UserMingXiController {
 
-
+    private static final Log log = LogFactory.getLog(UserMingXiController.class);
     @Resource
     private IUserService userService;
 
@@ -27,6 +30,7 @@ public class UserMingXiController {
      */
     @RequestMapping("/getUserMingxiByRelate_id")
     public List<UserMingxi> getUserMingxiByRelate_id(int relate_id){
+        log.info(" start");
         return userService.getUserMingxiByRelate_id(relate_id);
     }
 
